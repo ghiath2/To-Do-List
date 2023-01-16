4debb8e90947d7820d931cbda4f678fbf18dff48
ghiath2 <ghiathht3@gmail.com>
2023-01-16 21:05:27 +0000

    Create add item file

--- todo/views.py
+++ todo/views.py
@@ -12,4 +12,17 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
-    
+
+def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+
